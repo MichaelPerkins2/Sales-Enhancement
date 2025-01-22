26b29b4366ff74cb8c0de9ae57c6ea791bfc82e5
Michael Perkins <michael.perkins.d@gmail.com>
2025-01-22 14:44:30 -0700

    Added generation logic

--- app.py
+++ app.py
@@ -3,24 +3,10 @@ from flask import Flask, request, jsonify, render_template, redirect, url_for
 from datetime import datetime, timedelta
 import sqlite3
 import secrets
+import random
+import string
 
 app = Flask(__name__)
-
-# @app.route('/success/<name>')
-# def success(name):
-#     return 'Welcome %s!' % name
-
-# @app.route('/login', methods=['POST', 'GET'])
-# def login():
-#     if request.method == 'POST':
-#         user = request.form['nm']
-#         return redirect(url_for('success', name=user))
-#     else:
-#         return render_template('login.html')
-
-'''
-Project code below:
-'''
 
 # Routes
 @app.route('/validator')
@@ -38,12 +24,35 @@ def validate_coupon():
 def generator():
     return render_template('generator.html')
 
-@app.route('/generate_coupon', methods=['POST'])
+@app.route('/api/coupons/generate', methods=['POST'])
 def generate_coupon():
     data = request.get_json()
-    coupon_data = data['coupon_data']
-    # TODO: Coupon generation logic
-    return
+    
+    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+
+    conn = sqlite3.connect('coupons.db')
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute('''
+            INERT INTO coupons (code, type, valid_from, valid_until, used)
+            VALUES (?, ?, ?, ?, FALSE)              
+        ''', (code, data['type'], data['valid_from'], data['valid_until']))
+
+        conn.commit()
+
+        return jsonify({
+            'code': code,
+            'type': data['type'],
+            'valid_from': data['valid_from'],
+            'valid_until': data['valid_until']
+        }), 201
+    
+    except sqlite3.Error as e:
+        print(e)
+        return jsonify({'error': 'Error generating coupon'}), 500
+    finally:
+        conn.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
